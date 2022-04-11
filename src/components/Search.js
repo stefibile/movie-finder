884f7203be0e2e9ef27c116c386cc220c60d2230
Stefania Bielewicz <stefaniabielewicz@gmail.com>
2022-04-10 22:22:13 -0300

    adding responsive

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,24 @@ import SearchContainer from "./SearchContainer";
 
 const Search = () => {
   const [searchValue, setSearch] = useState("");
-  const findValue = (event) => setSearch(event.target.value);
+  const findValue = (event) => {
+    setSearch(event.target.value);
+    const container = document.querySelector(".input-container");
+    if (searchValue !== "") container.classList.remove("no-search");
+  };
 
   return (
-    <div className="input-container">
-        <input type="text" className="search-input" onInput={findValue} placeholder="Find what you want here"/>
-        {/* Querida Male: No pude encontrar mejor manera de limitar el fetch con espacios vacíos que rompen la app */}
-        {searchValue !== "" && searchValue !== " " && searchValue !== "  " && <SearchContainer searchValue={searchValue} />}
+    <div className="input-container no-search">
+      <input
+        type="text"
+        className="search-input"
+        onInput={findValue}
+        placeholder="Find what you want here"
+      />
+      {/* Querida Male: No pude encontrar mejor manera de limitar el fetch con espacios vacíos que rompen la app */}
+      {searchValue !== "" && searchValue !== " " && searchValue !== "  " && (
+        <SearchContainer searchValue={searchValue} />
+      )}
     </div>
   );
 };
